23999fddf24fda68d9c78c30643f4b40b2eeeebc
Clark Domanais <clarkdomanais@gmail.com>
2026-03-12 00:46:15 +0800

    added a condition where it prints the number of even numbers in the list

--- print_even_numbers.py
+++ print_even_numbers.py
@@ -4,3 +4,7 @@ numbers = []
 for i in range(10):
     num = int(input("Please enter a number: "))
     numbers.append(num)
+
+#Print how many the even numbers are from the list
+even_numbers = [num for num in numbers if num % 2 == 0]
+print("The number of even numbers is:", len(even_numbers))
