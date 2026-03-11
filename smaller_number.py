1747eb7f0f1b2d387f391c313e46ae5e32bb3dbb
Clark Domanais <clarkdomanais@gmail.com>
2026-03-12 00:39:11 +0800

    added a condition where it compares the 2 numbers and prints the smaller number

--- smaller_number.py
+++ smaller_number.py
@@ -2,3 +2,9 @@
 #Ask user to input 2 numbers   
 num_1 =  int(input("Please enter a number: "))
 num_2 = int(input("Please enter a number: "))
+
+#Compare the 2 numbers and print the smaller one
+if num_1 < num_2:
+    print(num_1, "is the smaller number.")
+else:
+    print(num_2, "is the smaller number.")
