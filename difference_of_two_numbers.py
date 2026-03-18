66ec63ac71a3b65e19b558fa3158a6587d186c52
Clark Domanais <clarkdomanais@gmail.com>
2026-03-18 19:12:25 +0800

    added a condition where it determines the difference of the two numbers

--- difference_of_two_numbers.py
+++ difference_of_two_numbers.py
@@ -2,3 +2,7 @@
 #Ask the user to input 2 numbers
 num_1 = int(input("Please enter a number: "))
 num_2 = int(input("Please enter a number: "))
+
+#Determine the difference between the 2 numbers that's been inputted
+difference = abs(num_1 - num_2) 
+print("The difference between the two numbers is:", difference)
