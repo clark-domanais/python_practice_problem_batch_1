58fe814d17c69e8784ae9cd620fc785fee1d9114
Clark Domanais <clarkdomanais@gmail.com>
2026-03-18 19:28:27 +0800

    added a condition where the first number is divided by the second number and also changed the string in the input variable

--- first_number_divided_by_the_second_number.py
+++ first_number_divided_by_the_second_number.py
@@ -3,3 +3,7 @@
 num_1 = int(input("Please enter a number: "))
 num_2 = int(input("Please enter a number: "))
 
+#Divide the first number by the second number and print the result
+if num_2 != 0:
+    result = num_1 / num_2
+    print("The result of dividing the first number by the second number is: ", result)
