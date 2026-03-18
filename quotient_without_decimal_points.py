28e5d32b5ffeac5443b8a70c79b03f0de97c43ec
Clark Domanais <clarkdomanais@gmail.com>
2026-03-18 19:22:00 +0800

    added a condition where it gets the quotient of the two numbers without decimal points

--- quotient_without_decimal_points.py
+++ quotient_without_decimal_points.py
@@ -3,3 +3,7 @@
 num_1 = int(input("Please enter a number: "))
 num_2 = int(input("Please enter a number: "))
 
+#Determine the quotient of the 2 numbers that's been inputted without decimal points
+if num_2 != 0:
+    quotient = num_1 // num_2
+    print("The quotient of the two numbers is: ", quotient)
