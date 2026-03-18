f6171c5e984c8e5bcdb3d0372aa21dd31b06b955
Clark Domanais <clarkdomanais@gmail.com>
2026-03-18 23:18:02 +0800

    added a condition where the system prints the numbers 0 to 100 and print the odd numbers in the list

--- odd_numbers_from_one_to_one-hundred.py
+++ odd_numbers_from_one_to_one-hundred.py
@@ -3,3 +3,10 @@
 numbers = []
 for i in range(1, 101):
     numbers.append(i)
+    
+#Print the odd numbers from 1 to 100 using while loop
+index = 0
+while index < len(numbers):
+    if numbers[index] % 2 != 0:
+        print(numbers[index])
+    index += 1
